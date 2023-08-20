91ae9e53f2970ab86fa36e7558d9c44ee9907838
Lucas Lu <lucas89901@gmail.com>
2023-08-20 14:20:28 +0800

    Add script that refreshes course selection page when timeout occurs

---
+++ js/timeout_refresh.js
@@ -0,0 +1,22 @@
+const divMain = document.getElementById('div-main');
+let seconds_left = 3;
+
+function hintText() {
+  return `Timout message detected. Automatic refresh in ${
+      seconds_left} seconds...`;
+}
+
+if (divMain.innerText.includes('使用時間超過, 請關掉瀏覽器後重新登入系統 !')) {
+  const divHint = document.createElement('div');
+  divHint.id = 'hint';
+  divHint.innerText = hintText();
+  divMain.insertAdjacentElement('beforeend', divHint);
+
+  const timer = setInterval(() => {
+    if (--seconds_left == 0) {
+      window.location.href = `${window.location.origin}/rtcourse/rtcourse.php`;
+      clearInterval(timer);
+    }
+    divHint.innerText = hintText();
+  }, 1000);
+}
