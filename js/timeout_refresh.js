aaef184061ba5067f6085081ccad406f52261989
Lucas Lu <lucas89901@gmail.com>
2024-08-21 22:14:46 +0800

    Fix typo

--- js/timeout_refresh.js
+++ js/timeout_refresh.js
@@ -2,7 +2,7 @@ const divMain = document.getElementById('div-main');
 let seconds_left = 3;
 
 function hintText() {
-  return `Timout message detected. Automatic refresh in ${
+  return `Timeout message detected. Automatic refresh in ${
       seconds_left} seconds...`;
 }
 
@@ -10,8 +10,7 @@ if (divMain &&
     divMain.innerText.includes('使用時間超過, 請關掉瀏覽器後重新登入系統 !')) {
   const divHint = document.createElement('div');
   divHint.id = 'hint';
-  divHint.innerText = `Timout message detected. Automatic refresh in ${
-      seconds_left} seconds...`;
+  divHint.innerText = hintText();
   divMain.insertAdjacentElement('beforeend', divHint);
 
   const timer = setInterval(() => {
